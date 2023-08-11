507e0bcc951c67b0f9a53d46912b32bd701525a5
fesily <fesil@foxmail.com>
2023-08-11 17:33:27 +0800

    auto copy files

--- Mod/modinfo.lua
+++ Mod/modinfo.lua
@@ -10,7 +10,7 @@ description = [[
 
 author = "fesil"
 
-version = "0.0.9"
+version = "0.1.0"
 
 forumthread = "https://github.com/fesily/DontStarveLuaJit2"
 
--- Mod/modmain.lua
+++ Mod/modmain.lua
@@ -1,24 +1,12 @@
-local _G = GLOBAL
-if not _G.rawget(_G, "jit") then
-    local PopupDialogScreen = require "screens/popupdialog"
+_G = GLOBAL
 
-    _G.TheFrontEnd:PushScreen(
-        PopupDialogScreen(
-            "Warning",
-            "LuaJIT模块没有加载\nLuaJIT MOD is not installed!",
-            {
-                {
-                    text = _G.STRINGS.UI.MAINSCREEN.YES,
-                    cb = function()
-                        _G.TheFrontEnd:PopScreen()
-                    end
-                }
-            }
-        )
-    )
+if not _G.rawget(_G, "jit") then
+    modimport "installer"
+    _G.TheSim:Quit()
     return
 end
-
-AddGamePostInit(function()
-    _G.STRINGS.UI.MAINSCREEN.MODTITLE = "LuaJIT:" .. _G.STRINGS.UI.MAINSCREEN.MODTITLE
-end)
+local TEMPLATES = require "widgets/redux/templates"
+local old_getbuildstring = TEMPLATES.GetBuildString
+TEMPLATES.GetBuildString = function()
+    return (old_getbuildstring() or "") .. "(LuaJIT)"
+end
