016e639461cb99fbb5bacd6baf8ecf0789e708cd
fesily <fesil@foxmail.com>
2024-02-20 22:54:25 +0800

    fix arg bug

---
+++ tests/arg_bug.lua
@@ -0,0 +1,38 @@
+local function test(...)
+    local arg = {...}
+    arg[1] = 2
+    assert(arg[1] == 2)
+    if 1 then
+        assert(arg[1] == 2)
+    end
+    local limit = true
+    while limit do
+        assert(arg[1] == 2)
+        limit = false
+    end
+    do
+        assert(arg[1] == 2)
+    end
+    repeat
+        assert(arg[1] == 2)
+    until true
+end
+local function test1(...)
+    assert(arg[1] == 2)
+    if 1 then
+        assert(arg[1] == 2)
+    end
+    local limit = true
+    while limit do
+        assert(arg[1] == 2)
+        limit = false
+    end
+    do
+        assert(arg[1] == 2)
+    end
+    repeat
+        assert(arg[1] == 2)
+    until true
+end
+test(1)
+test1(2)
