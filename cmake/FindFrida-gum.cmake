6e2fbabb6cce48fe30b9aebe3f5e72e1d02361ae
fesil <fesil@foxmail.com>
2024-02-24 10:16:53 +0800

    fix build error

--- cmake/FindFrida-gum.cmake
+++ cmake/FindFrida-gum.cmake
@@ -16,7 +16,11 @@ find_library(FRIDA_GUM_LIBRARIES
     PATHS ${PROJECT_SOURCE_DIR}/3rd/frida-gum/${FRIDA_GUM_LIBRARIES_PREFIX}
     REQUIRED)
 
+get_filename_component(FRIDA_GUM_LIBRARY_DIR "${FRIDA_GUM_LIBRARIES}" DIRECTORY)
+
+add_compile_definitions(GUM_STATIC=1)
+
 include(FindPackageHandleStandardArgs)
 # Check if the library has been found
 find_package_handle_standard_args(Frida-gum DEFAULT_MSG
-    FRIDA_GUM_LIBRARIES FRIDA_GUM_INCLUDE_DIR)
+    FRIDA_GUM_LIBRARIES FRIDA_GUM_INCLUDE_DIR FRIDA_GUM_LIBRARY_DIR)
