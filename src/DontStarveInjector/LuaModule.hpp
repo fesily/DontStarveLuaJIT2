a0a886bb0f01fb0115583eab87507b3d57d5f535
fesil <fesil@foxmail.com>
2024-02-25 21:53:07 +0800

    update

--- src/DontStarveInjector/LuaModule.hpp
+++ src/DontStarveInjector/LuaModule.hpp
@@ -3,13 +3,21 @@
 #include "frida-gum.h"
 #include "util/Signature.hpp"
 
-static MemorySignature luaModuleSignature{"41 B8 EE D8 FF FF 41 3B D0 74 64 81 FA EF D8 FF FF 74 3B", -0x37};
-
-static MemorySignature luaRegisterDebugGetsizeSignature{"4C 8B 5B 18 48 8B CB 49 8B", 
-#if DEBUG_GETSIZE_PATCH == 1
-    0x7
+static MemorySignature luaModuleSignature{
+#ifdef _WIN32
+    "41 B8 EE D8 FF FF 41 3B D0 74 64 81 FA EF D8 FF FF 74 3B", -0x37
 #else
-    -0x27
+    "81 FE F1 D8 FF FF 7D 60 81 FE EF D8 FF FF 74 78 81 FE F0 D8 FF FF", -0x28
+#endif
+};
+
+static MemorySignature luaRegisterDebugGetsizeSignature
+{
+    "4C 8B 5B 18 48 8B CB 49 8B",
+#if DEBUG_GETSIZE_PATCH == 1
+        0x7
+#else
+        -0x27
 #endif
 };
 
