39e65f7df6c66dbaa1c450f8f907841c16d4c42d
fesily <fesil@foxmail.com>
2025-03-08 08:46:48 +0800

    test: update unpack

--- tests/unpack.lua
+++ tests/unpack.lua
@@ -17,3 +17,16 @@ assert(t1[3] == 3)
 t1 = { unpack({ 1, 2, 3, nil, 5, nil, nil, 8, nil }) }
 assert(t1[8] == nil)
 assert(t1[3] == 3)
+
+
+function foo(...)
+	return {...}
+end
+
+t1 = { unpack({ 1, 2, 3, nil, 5 }) }
+assert(t1[5] == 5)
+assert(select(5, unpack(t1))  == 5)
+
+t1 = foo(1, 2, 3, nil, 5)
+assert(t1[5] == 5)
+assert(select(5, unpack(t1))  == 5)
