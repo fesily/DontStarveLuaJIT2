a2f7e697cdf1c4b115f041182117d383c9e989cf
fesily <fesil@foxmail.com>
2026-08-07 17:56:06 +0800

    build(gum): stage shared Frida::Gum via setup_frida_gum

--- cmake/FindFrida-gum.cmake
+++ cmake/FindFrida-gum.cmake
@@ -1,26 +1,58 @@
 if (WIN32)
-    Set(FRIDA_GUM_LIBRARIES_PREFIX "win64")
-elseif(APPLE)
-    Set(FRIDA_GUM_LIBRARIES_PREFIX "osx")
+    set(_FRIDA_GUM_PLAT "win64")
+elseif (APPLE)
+    set(_FRIDA_GUM_PLAT "osx")
 else()
-    Set(FRIDA_GUM_LIBRARIES_PREFIX "linux64")
+    set(_FRIDA_GUM_PLAT "linux64")
 endif()
 
-find_path(FRIDA_GUM_INCLUDE_DIR 
-    NAMES frida-gum.h 
-    PATHS ${PROJECT_SOURCE_DIR}/3rd/frida-gum/${FRIDA_GUM_LIBRARIES_PREFIX}
+set(_FRIDA_GUM_ROOT "${PROJECT_SOURCE_DIR}/3rd/frida-gum/${_FRIDA_GUM_PLAT}")
+
+find_path(FRIDA_GUM_INCLUDE_DIR
+    NAMES frida-gum.h
+    PATHS
+        "${_FRIDA_GUM_ROOT}/include"
+        "${_FRIDA_GUM_ROOT}"
+    NO_DEFAULT_PATH
     REQUIRED)
 
-find_library(FRIDA_GUM_LIBRARIES
-    NAMES frida-gum.lib libfrida-gum.a
-    PATHS ${PROJECT_SOURCE_DIR}/3rd/frida-gum/${FRIDA_GUM_LIBRARIES_PREFIX}
-    REQUIRED)
+if (WIN32)
+    find_library(FRIDA_GUM_LIBRARIES
+        NAMES frida-gum
+        PATHS "${_FRIDA_GUM_ROOT}/lib"
+        NO_DEFAULT_PATH
+        REQUIRED)
+    find_file(FRIDA_GUM_RUNTIME
+        NAMES frida-gum.dll
+        PATHS "${_FRIDA_GUM_ROOT}/bin" "${_FRIDA_GUM_ROOT}/lib"
+        NO_DEFAULT_PATH
+        REQUIRED)
+else()
+    find_library(FRIDA_GUM_LIBRARIES
+        NAMES frida-gum
+        PATHS "${_FRIDA_GUM_ROOT}/lib"
+        NO_DEFAULT_PATH
+        REQUIRED)
+    set(FRIDA_GUM_RUNTIME "${FRIDA_GUM_LIBRARIES}")
+endif()
 
 get_filename_component(FRIDA_GUM_LIBRARY_DIR "${FRIDA_GUM_LIBRARIES}" DIRECTORY)
 
-add_compile_definitions(GUM_STATIC=1)
+if (NOT TARGET Frida::Gum)
+    add_library(Frida::Gum SHARED IMPORTED GLOBAL)
+    set_target_properties(Frida::Gum PROPERTIES
+        IMPORTED_LOCATION "${FRIDA_GUM_RUNTIME}"
+        INTERFACE_INCLUDE_DIRECTORIES "${FRIDA_GUM_INCLUDE_DIR}"
+    )
+    if (WIN32)
+        set_target_properties(Frida::Gum PROPERTIES
+            IMPORTED_IMPLIB "${FRIDA_GUM_LIBRARIES}"
+        )
+    endif()
+endif()
+
+# Shared consumers: do NOT define GUM_STATIC (dllimport on Windows).
 
 include(FindPackageHandleStandardArgs)
-# Check if the library has been found
 find_package_handle_standard_args(Frida-gum DEFAULT_MSG
-    FRIDA_GUM_LIBRARIES FRIDA_GUM_INCLUDE_DIR FRIDA_GUM_LIBRARY_DIR)
+    FRIDA_GUM_LIBRARIES FRIDA_GUM_INCLUDE_DIR FRIDA_GUM_RUNTIME)
