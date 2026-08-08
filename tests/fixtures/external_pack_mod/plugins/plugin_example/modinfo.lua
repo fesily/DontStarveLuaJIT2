28152f8ed7f66e77482c912bd27d907c9cdee063
fesily <fesil@foxmail.com>
2026-08-08 17:28:33 +0800

    test(fixtures): complete external pack example package tree

---
+++ tests/fixtures/external_pack_mod/plugins/plugin_example/modinfo.lua
@@ -0,0 +1,14 @@
+name = "Example Package Face"
+description = "Package face fixture"
+author = "DontStarveLuaJit2"
+version = "1.0.0"
+api_version = 10
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+dst_compatible = true
+client_only_mod = false
+server_only_mod = false
+all_clients_require_mod = false
+plugin_id = "example.external"
+phases = "AfterModMain"
+priority = 100
